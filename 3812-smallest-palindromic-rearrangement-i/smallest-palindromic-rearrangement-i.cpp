84a100929eff1dab64baf4b94bd59cb36a334b52
Deepak <deepakyadav0642@gmail.com>
2026-07-28 07:06:46 +0530

    Time: 30 ms (69.23%) | Memory: 60.9 MB (80.00%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    string smallestPalindrome(string s) 
-    {
-        string g(s.length() , '/');
+    string smallestPalindrome(string s) {
         int n = s.length();
-        if(n%2!=0){
-            g[(n/2)] = s[(n/2)];
+        string g(n, '/');
+        
+        // Correct: The middle of an existing palindrome is the odd character
+        if (n % 2 != 0) {
+            g[n / 2] = s[n / 2]; 
         }
-        map <char , int> m;
-        for(int  i  = 0 ; i<n ; i++){
-            if(g[i] == '/'){
-            m[s[i]]++;}
-        }
-        int strt = 0;
-        vector<pair<char , int>> v;
-        for(auto i:m){
-            v.push_back(make_pair(i.first , i.second));
-        }
-        for(int i = 0 ; i<v.size() ; i++){
-            g[strt] = v[i].first;
-            g[n-strt - 1] = v[i].first;
-            v[i].second -= 2;
-            if(v[i].second>0){
-                i--;
+        
+        vector<int> v(26, 0);
+        for (int i = 0; i < n; i++) {
+            // FIX 1: Check g[i], not s[i]
+            if (g[i] == '/') {
+                v[s[i] - 'a']++; 
             }
-            strt++;
+        }
+        
+        int strt = 0;
+        for (int i = 0; i < v.size(); i++) {
+            // FIX 3: Only process characters that actually exist to be placed
+            if (v[i] >= 2) {
+                // FIX 2: Use index 'i' to get the character, not the frequency 'v[i]'
+                g[strt] = 'a' + i;
+                g[n - strt - 1] = 'a' + i;
+                
+                v[i] -= 2;
+                
+                if (v[i] > 0) {
+                    i--;
+                }
+                strt++;
+            }
         }
         return g;
     }
