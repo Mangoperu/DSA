caca1f0ff90e4b0a0357cbdeaf6ec24a31290b2b
Deepak <deepakyadav0642@gmail.com>
2026-07-29 21:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (70.06%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-               int n = nums.size();
-          unordered_map<int , int> m;
-        for(int i = 0 ; i<n ; i++){
-            m[nums[i]]++;
-            if(m[nums[i]] == 2){
-                return nums[i];
-            }
+        // Phase 1: Detect if a cycle exists (it always will in this problem)
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];           // Move 1 step
+            fast = nums[nums[fast]];     // Move 2 steps
+        } while (slow != fast);
+        
+        // Phase 2: Find the entrance to the cycle (the duplicate number)
+        slow = nums[0]; // Reset slow pointer to the start
+        
+        while (slow != fast) {
+            slow = nums[slow];           // Move 1 step
+            fast = nums[fast];           // Move 1 step
         }
-        return -1;
+        
+        return slow; // Both pointers meet at the duplicate number
     }
 };
