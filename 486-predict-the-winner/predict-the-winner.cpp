2909fe314c651eb6e069e3caba2f35edaae4e50b
Deepak <deepakyadav0642@gmail.com>
2026-08-01 08:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (52.91%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int rec(int i , int j , vector<int> &nums){
+    int rec(int i , int j , vector<int> &nums, vector<vector<int>>&v){
         if(i == j){
             return nums[i];
         }
         if(i>j){
             return 0;
         }
-        int take1 = nums[i] + min(rec( i+2 , j , nums) , rec(i+1 , j-1 , nums));
-        int take2 = nums[j] + min(rec(i+1 , j-1 , nums) , rec(i , j-2 , nums));
+        if(v[i][j] != -1){
+            return v[i][j];
+        }
+        int take1 = nums[i] + min(rec( i+2 , j , nums,v) , rec(i+1 , j-1 , nums,v));
+        int take2 = nums[j] + min(rec(i+1 , j-1 , nums,v) , rec(i , j-2 , nums,v));
+        v[i][j] = max(take1 , take2);
         return max(take1 , take2);
     }
     bool predictTheWinner(vector<int>& nums) {
+        vector<vector<int>> v(21 , vector<int>(21 , -1));
         int total  = accumulate(nums.begin() , nums.end() , 0);
-        int p2 = total - rec(0,nums.size()-1 , nums);
-        if(rec(0,nums.size()-1 , nums)>=p2){
+        int p2 = total - rec(0,nums.size()-1 , nums , v);
+        
+        if(rec(0,nums.size()-1 , nums , v)>=p2){
             return true;
         }
         else{
