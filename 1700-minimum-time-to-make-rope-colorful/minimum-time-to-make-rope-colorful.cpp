4092471c2d2937db8a5cd0a42e9b2f4498e3a32a
Deepak <deepakyadav0642@gmail.com>
2026-08-02 21:02:22 +0530

    Time: 4 ms (57.14%) | Memory: 99.9 MB (32.52%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int rec(string & c , vector<int>&t , char last , int ind , vector<vector<int>>& dp){
-        if(ind == c.length()){
-            return 0;
-        }
-        int lasti = (last == '0') ? 26 : (last - 'a');
-        if(dp[ind][lasti] != -1){
-            return dp[ind][lasti];
-        }
-        int pick = 1e9;
-        if(last != c[ind]){
-        pick = rec(c , t, c[ind] , ind+1 , dp);}
-        int didnt =  t[ind] + rec(c, t , last , ind+1 , dp);
-        dp[ind][lasti] = min(pick , didnt);
-        return dp[ind][lasti];
-    }
     int minCost(string colors, vector<int>& neededTime) {
-        vector<vector<int>> dp(colors.length() , vector<int>(27 , -1));
-        return  rec(colors , neededTime , '0' , 0 , dp);
+        int n = colors.size(), sum = 0;
+        
+        for (int i = 1; i < n; i++) {
+            int maxi = 0; // largest value balloon in substring
+            while (i < n and colors[i] == colors[i - 1]) {
+                sum += neededTime[i - 1];
+                maxi = max(maxi, neededTime[i - 1]);
+                ++i;
+            }
+            sum += neededTime[i - 1];
+            maxi = max(maxi, neededTime[i - 1]);
+            if (maxi != 0) sum -= maxi;
+        }
+        return sum;
     }
 };
