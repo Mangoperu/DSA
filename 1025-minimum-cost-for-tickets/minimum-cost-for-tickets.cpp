fa5973bd8792f71ae4560b31b03afd45a3eb7f83
Deepak <deepakyadav0642@gmail.com>
2026-08-02 20:35:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (96.70%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -8,17 +8,17 @@ public:
         if(a>=d[b]){
           return rec(d , c , a , b+1,v);
         }
-        if(v[b]!= -1){
-            return v[b];
+        if(v[a]!= -1){
+            return v[a];
         }
         int one = c[0]+rec(d , c , d[b] , b+1,v );
         int seven = c[1] + rec(d , c, d[b]+6 , b+1,v);
         int thity = c[2] + rec( d , c ,d[b] + 29 , b+1,v);
-        v[b] = min(min(one  , seven) , thity);
-        return v[b];
+        v[a] = min(min(one  , seven) , thity);
+        return v[a];
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int> v(366 , -1);
+        vector<int> v(400 , -1);
         return rec( days , costs , 0 , 0 , v);
 
     }
