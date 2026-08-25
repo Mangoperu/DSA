be3d9dd207440958f2dbeb9ce01331421dfb89f7
Deepak <deepakyadav0642@gmail.com>
2026-08-25 07:46:55 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (79.36%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int totalSum = 0;
-        for (int num : nums) totalSum += num;
-
-        if (totalSum % 2 != 0) return false;
-
-        int targetSum = totalSum / 2;
-        vector<bool> dp(targetSum + 1, false);
-        dp[0] = true;
-        for (int num : nums) {
-            for (int currSum = targetSum; currSum >= num; --currSum) {
-                dp[currSum] = dp[currSum] || dp[currSum - num];
-                if (dp[targetSum]) return true;
-            }
+        int sum = 0;
+        for(int num : nums) {
+            sum += num;
         }
-        return dp[targetSum];
+        
+        if(sum % 2 != 0) return false;
+        
+        int target = sum / 2;
+        
+        // LeetCode constraints: 
+        // 1 <= nums.length <= 200, 1 <= nums[i] <= 100
+        // Max possible sum = 20000, Max target = 10000
+        bitset<10001> dp;
+        dp[0] = 1; // Base case: we can always make a sum of 0
+        
+        for(int num : nums) {
+            // Shift the bits to the left by 'num' and combine with current dp
+            dp = dp | (dp << num);
+        }
+        
+        return dp[target];
     }
 };
