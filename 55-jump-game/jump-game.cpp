fc872ebf733a0c42e5a9200626c0adb0c7b5a4a1
Deepak <deepakyadav0642@gmail.com>
2026-08-26 11:14:59 +0530

    Time: 167 ms (14.36%) | Memory: 55.7 MB (7.57%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,21 +4,32 @@ public:
         if(ind+n[ind]>=n.size()-1){
             return true;
         }
-        bool ans = 0;
-        if(dp[ind]!=-1){
+        
+        // 1. Check if we ALREADY solved for this index
+        if(dp[ind] != -1){
             return dp[ind];
         }
+        
+        bool ans = 0;
         for(int i = 1 ; i<=n[ind] ; i++){
             if(dp[ind+i]!=-1){
                 ans |= dp[ind+i];
+            } else {
+                // 2. Only recurse if we haven't solved it yet
+                ans |= rec(ind+i , n , dp);
             }
-            ans |= rec(ind+i , n , dp);
+            
+            // 3. Early exit: stop checking other jumps if we already found a path
+            if(ans) break; 
         }
-        dp[ind] = ans;
-        return ans;
         
+        // 4. Save the answer in the dp array before returning!
+        return dp[ind] = ans; 
     }
+    
     bool canJump(vector<int>& nums) {
+        if(nums.size() <= 1) return true; // Safety check for arrays of size 1
+        
         vector<int>dp(nums.size() , -1);
         return rec(0,nums,dp);
     }
