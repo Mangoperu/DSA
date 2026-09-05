65335f428dec4abd1e23ccc62a65578669affe32
Deepak <deepakyadav0642@gmail.com>
2026-09-05 12:02:11 +0530

    Time: 4 ms (89.30%) | Memory: 193.7 MB (99.57%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        int n = nums.size();
-        if (n == 0) return -1;
-        
-        // 1. Build prefix max array
-        vector<int> v(n);
-        int maxi = INT_MIN;
-        for(int i = 0 ; i < n ; i++) {
-             maxi = max(maxi, nums[i]);
-             v[i] = maxi;
+        int maxSoFar = -1;
+        int cand = 0, cmax = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            maxSoFar = max(maxSoFar, nums[i]);
+
+            if (i == cand) cmax = maxSoFar;
+
+            if (nums[i] < cmax - k)
+                cand = i + 1;
         }
-        
-        // 2. Build suffix min array
-        vector<int> m(n);
-        int mini = INT_MAX; 
-        for(int i = n - 1; i >= 0; i--) {
-            mini = min(nums[i], mini);
-            m[i] = mini; // Assigned directly to the matching index
-        }
-        
-        // 3. Find the SMALLEST index that is stable
-        for(int i = 0 ; i < n; i++) {
-            int score = v[i] - m[i]; 
-            if(score <= k) {
-                return i; // Return immediately! This is the smallest index.
-            }
-        }
-        
-        return -1; // No stable index found
+
+        return cand < nums.size() ? cand : -1;
     }
 };
